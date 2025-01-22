2b0764713fe257f6b09cfcd79bec64453c0db6da
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-01-22 19:08:40 +0100

    Add styling to navigation bar

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
+    const menu = document.querySelector('.navbar');
     const menuItems = document.querySelector('.navbar-items');
     const hamburgerIcon = document.querySelector('.hamburger-icon');
 
     hamburgerIcon.addEventListener('click', function () {
+        menu.classList.toggle('show');
         menuItems.classList.toggle('show');
         hamburgerIcon.classList.toggle('active');
     });
