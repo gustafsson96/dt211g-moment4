33af0e562ed194a5d91bb6af0c231f2b72c90ea1
Julia Gustafsson <juliagustafsson96@hotmail.com>
2025-01-22 17:06:31 +0100

    Create navigation bar for smaller screens

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-console.log("Hello world!")
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuItems = document.querySelector('.navbar-items');
+    const hamburgerIcon = document.querySelector('.hamburger-icon');
+
+    hamburgerIcon.addEventListener('click', function () {
+        menuItems.classList.toggle('show');
+        hamburgerIcon.classList.toggle('active');
+    });
+});
